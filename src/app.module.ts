bb71d61eb80c0b9aad1cc5457f771374c90cadc2
amIAqua <sidelnikov224@gmail.com>
2021-07-12 12:42:36 +0300

    services/configService, app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ClientProxyFactory } from '@nestjs/microservices'
+
+import { ConfigService } from './services/config/config.service'
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [
+    {
+      provide: 'CLIENT_SERVICE',
+      useFactory: (configService: ConfigService) => {
+        const clientServiceOptions = configService.get('clientService')
+        return ClientProxyFactory.create(clientServiceOptions)
+      },
+      inject: [ConfigService],
+    },
+    {
+      provide: 'APPLICATION_SERVICE',
+      useFactory: (configService: ConfigService) => {
+        const applicationServiceOptions =
+          configService.get('applicationService')
+        return ClientProxyFactory.create(applicationServiceOptions)
+      },
+      inject: [ConfigService],
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/services/config/config.service.ts
@@ -0,0 +1,30 @@
+import { Transport } from '@nestjs/microservices'
+
+export class ConfigService {
+  private readonly envConfig: { [key: string]: any } = null
+
+  constructor() {
+    this.envConfig = {}
+
+    this.envConfig.port = process.env.API_GATEWAY_PORT
+
+    ;(this.envConfig.clientService = {
+      options: {
+        port: process.env.CLIENT_SERVICE_PORT,
+        host: process.env.CLIENT_SERVICE_HOST,
+      },
+      transport: Transport.TCP,
+    }),
+      (this.envConfig.applicationService = {
+        options: {
+          port: process.env.APPLICATION_SERVICE_PORT,
+          host: process.env.APPLICATION_SERVICE_HOST,
+        },
+        transport: Transport.TCP,
+      })
+  }
+
+  get(key: string) {
+    return this.envConfig[key]
+  }
+}

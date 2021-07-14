3f38eb6c4d660279a73696bbf7295d3981de6ac1
amIAqua <sidelnikov224@gmail.com>
2021-07-14 10:01:10 +0300

    dev commit

--- src/client.controller.ts
+++ src/client.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get, Inject, Post } from '@nestjs/common'
+import { Controller, Get, HttpException, HttpStatus, Inject, Param, Post } from '@nestjs/common'
 import { ClientProxy } from '@nestjs/microservices'
+
+import { IGetClientByIdResponse } from './interfaces/client/get-client-by-id-pesponse.interface'
 
 @Controller('clients')
 export class ClientController {
-  constructor(
-    @Inject('CLIENT_SERVICE') private readonly clientServiceClient: ClientProxy,
-  ) {}
+  constructor(@Inject('CLIENT_SERVICE') private readonly clientServiceClient: ClientProxy) {}
 
-  @Post()
-  public async createClient() {}
+  @Get('/:id')
+  public async getClientById(@Param('id') id: string) {
+    const getClientByIdResponse: IGetClientByIdResponse = await this.clientServiceClient.send('client_find_by_id', id).toPromise()
+
+    if (getClientByIdResponse.status !== HttpStatus.OK) {
+      if (getClientByIdResponse.status !== HttpStatus.OK) {
+        throw new HttpException(
+          {
+            error: getClientByIdResponse.error,
+          },
+          getClientByIdResponse.status,
+        )
+      }
+    }
+
+    return {
+      status: getClientByIdResponse.status,
+      client: getClientByIdResponse.client,
+    }
+  }
+
+  public
 }
